5d22f8cbd4a4287bfbae9c7784abb7cfe1224c7e
Trevor Nielsen <gooseleader@geese.wtf>
2022-02-20 22:14:58 -0800

    try fix for name not updating

--- src/plugin.ts
+++ src/plugin.ts
@@ -52,26 +52,6 @@ class RadiacodePlugin implements AccessoryPlugin {
     // HomeKit Air Quality Service
     this.airQualityService = new api.hap.Service.AirQualitySensor("Radiation Levels");
 
-    this.doseRateService = new api.hap.Service.OccupancySensor("Dose Rate");
-    this.doseRateService.getCharacteristic(api.hap.Characteristic.OccupancyDetected).onGet(async () => {
-      await this.getLatestSamples();
-      if (this.latestSamples.data['doserate'] && this.latestSamples.data['doserate'] >= 0.2) {
-        return 1
-      } else {
-        return 0;
-      }
-    });
-    this.doseRateService.getCharacteristic(api.hap.Characteristic.Name).onGet(async () => {
-      await this.getLatestSamples();
-      if (this.latestSamples.data['doserate']) {
-        return "Dose Rate: " + this.latestSamples.data['doserate'].toPrecision(3).toString() + " uSv hr"
-      } else {
-        return "Unknown";
-      }
-    });
-
-
-
     this.airQualityService.getCharacteristic(api.hap.Characteristic.AirQuality)
       .onGet(async () => {
         await this.getLatestSamples();
@@ -107,6 +87,20 @@ class RadiacodePlugin implements AccessoryPlugin {
         await this.getLatestSamples();
         return this.latestSamples.data.timestamp != null && Date.now() / 1000 - this.latestSamples.data.timestamp < 2 * 60 * 60;
       });
+    
+    //HomeKit Occupancy Sensor (For reporting Dose Rate)
+    this.doseRateService = new api.hap.Service.OccupancySensor("Dose Rate");
+    this.doseRateService.setCharacteristic(api.hap.Characteristic.Name, "Dose Rate: " + this.latestSamples.data['doserate']?.toPrecision(3).toString() + " uSv hr" ?? "Unknown")
+    
+    this.doseRateService.getCharacteristic(api.hap.Characteristic.OccupancyDetected).onGet(async () => {
+      await this.getLatestSamples();
+      this.doseRateService.setCharacteristic(api.hap.Characteristic.Name, "Dose Rate: " + this.latestSamples.data['doserate']?.toPrecision(3).toString() + " uSv hr" ?? "Unknown")
+      if (this.latestSamples.data['doserate'] && this.latestSamples.data['doserate'] >= 0.2) {
+        return 1
+      } else {
+        return 0;
+      }
+    });
   }
 
   getServices(): Service[] {
